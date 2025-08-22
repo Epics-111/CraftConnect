b25ae72252c223028c777863de1968209536c828
ammarvellous <ammaratif559@gmail.com>
2025-08-22 11:38:09 +0000

    Removed node backend codebase

--- backend/routes/userDetailsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { saveUserDetails } = require("../controllers/userDetailsController");
-const authMiddleware = require("../middleware/authMiddleware");
-
-const router = express.Router();
-
-router.post("/save-details", authMiddleware, saveUserDetails);
-
-module.exports = router;
